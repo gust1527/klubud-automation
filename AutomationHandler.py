052e8fe2b3b8dd21b398ee95d537f40e6fdf55bd
Gustav Emil Holm Simonsen <202204162@post.au.dk>
2023-06-07 22:42:35 +0200

    First commit to GitHub

--- AutomationHandler.py
+++ AutomationHandler.py
@@ -3,3 +3,6 @@ from dropbox_handler import browserSetup
 dropboxUrl = "https://dropbox.com"
 
 browserSetup(dropboxUrl)
+
+consumer_key = "ck_464ac420d491684bfa3cbbf95a774f6721fcd32c"
+consumer_secret = "cs_9d0963e25610f0ac9a65370654c567840f991874"
